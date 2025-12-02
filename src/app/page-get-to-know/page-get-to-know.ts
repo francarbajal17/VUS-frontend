bdab2912f7489b7f8a4c86193c3608fc74223bc6
Francisco Carbajal <francarba17@gmail.com>
2025-12-02 19:53:38 -0300

    loading 2 sec disabled, check if a size is selected to consult, size of loading logo and new g2k page

---
+++ src/app/page-get-to-know/page-get-to-know.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-page-get-to-know',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './page-get-to-know.html',
+  styleUrl: './page-get-to-know.css',
+})
+export class PageGetToKnow {}
